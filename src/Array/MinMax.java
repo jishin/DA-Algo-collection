df80abe272eed10d4f51eb0bdd81e62b684a11df
jishin <jishinpjijy@gmail.com>
2020-11-04 17:16:32 +0530

    ds algo code collection

--- src/Array/MinMax.java
+++ src/Array/MinMax.java
@@ -1,4 +1,45 @@
 package Array;
 
+import java.util.Scanner;
+
 public class MinMax {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter the number of elements");
+        int n= scn.nextInt();
+        System.out.println("Enter the array elements");
+        int ar[] = new int[n];
+        for(int i = 0; i<n;i++){
+            ar[i] = scn.nextInt();
+        }
+        int max = findMax(ar, n);
+        int min = findMin(ar, n);
+        System.out.println("Max element is: "+ max);
+        System.out.println("Min element is: "+ min);
+
+    }
+
+    static int findMax(int[] ar, int n){
+        int max=ar[0];
+        for(int i = 1; i<n;i++){
+            if(ar[i]>max){
+                max=ar[i];
+            }
+
+        }
+
+        return max;
+
+    }
+    static int findMin(int[] ar, int n){
+        int min=ar[0];
+        for(int i = 1; i<n;i++){
+            if(ar[i]<min){
+                min=ar[i];
+            }
+
+        }
+
+        return min;
+    }
 }
--- src/com/company/BubbleSort.java
+++ src/com/company/BubbleSort.java
@@ -38,4 +38,21 @@ public class BubbleSort {
         }
         System.out.println();
     }
+
+    public static int[] sort2(int arr[]){
+        int n=arr.length;
+        int temp=0;
+        for (int i=0;i<n-1;i++){
+            for (int j=0;j<n-i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+
+
+        return arr;
+    }
 }
